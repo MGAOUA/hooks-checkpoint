5be60992824ffcf740cfe60b0e762c7f563790c5
mgaoua <gaouaoui.meriem@gmail.com>
2021-08-27 11:12:04 +0100

    ajout choix

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,12 +4,39 @@ import { useState } from "react";
 import MovieCard from "./MovieCard";
 const Filter = ({ setfilmchoisis, filmchoisis }) => {
   const [Name, setName] = useState();
+  const [choix, setchoix] = useState(false);
+  const filmChoisi = () => {
+    if (Name) {
+      if (choix == "byfilm")
+        return setfilmchoisis(filmchoisis.filter((e) => e.title == Name));
 
-  const filmChoisi = () =>
-    setfilmchoisis(filmchoisis.filter((e) => e.title == Name));
+      if (choix == "byrate")
+        return setfilmchoisis(filmchoisis.filter((e) => e.rating == Name));
+    }
+    return filmchoisis;
+  };
 
   return (
     <div>
+      <div>
+        <input
+          type="radio"
+          name="choix"
+          onChange={(e) => setchoix(e.target.value)}
+          value="byfilm"
+        ></input>
+        <label>Name</label>
+        <br></br>
+
+        <input
+          type="radio"
+          name="choix"
+          onChange={(e) => setchoix(e.target.value)}
+          value="byrate"
+        ></input>
+        <label>Rate</label>
+      </div>
+
       <input onChange={(e) => setName(e.target.value)}></input>
       <button onClick={filmChoisi}>filtrer</button>
     </div>
